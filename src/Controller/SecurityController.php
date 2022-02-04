3a12adf4f6ef8045a809b6be4b7adf74241b134b
Kevin <ipandore45@gmail.com>
2022-02-04 12:32:40 +0100

    add register page

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,10 +2,18 @@
 
 namespace App\Controller;
 
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+use App\Entity\User;
+use App\Form\RegistrationFormType;
+use App\Security\AppCustomAuthenticator;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Security\Http\Authentication\UserAuthenticatorInterface;
 
 class SecurityController extends AbstractController
 {
@@ -31,33 +39,36 @@ class SecurityController extends AbstractController
         throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
     }
 
+
     #[Route('/register', name: 'app_register')]
-    public function new(Request $request, EntityManagerInterface $entityManager, UserPasswordHasherInterface $hasher): Response
+    public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, UserAuthenticatorInterface $userAuthenticator, AppCustomAuthenticator $authenticator, EntityManagerInterface $entityManager): Response
     {
-      if ($request->isMethod('POST')){
-          if (!empty($request->request->get('password'))
-              && !empty($request->request->get('password2'))
-              && $request->request->get('password') === $request->request->get ('password2')){
+        $user = new User();
+        $form = $this->createForm(RegistrationFormType::class, $user);
+        $form->handleRequest($request);
 
-              $user = new User();
-              $user->setFirstName ($request->request->get('firstName'))
-                  ->setLastName ($request->request->get('LastNane'))
-                  ->setEmail($request->request->get('email'))
-                  ->setPassword($hasher->hashPassword($user, $request->request->get('password')));
+        if ($form->isSubmitted() && $form->isValid()) {
+            // encode the plain password
+            $user->setPassword(
+            $userPasswordHasher->hashPassword(
+                    $user,
+                    $form->get('plainPassword')->getData()
+                )
+            );
 
-              $entityManager->persist($user);
-              $entityManager->flush();
+            $entityManager->persist($user);
+            $entityManager->flush();
+            // do anything else you need here, like send an email
 
-              return $this->redirectToRoute('app_user_show', [
-                'email' => $user->getEmail()
-              ]);
-          }
+            return $userAuthenticator->authenticateUser(
+                $user,
+                $authenticator,
+                $request
+            );
+        }
 
-          return $this->render('security/register.html.twig', [
-              'error' => 'Les deux password ne sont pas identiques'
-          ]);
-      }
-      return $this->render('security/register.htmt.twig') ;
+        return $this->render('security/register.html.twig', [
+            'registrationForm' => $form->createView(),
+        ]);
     }
-
 }
