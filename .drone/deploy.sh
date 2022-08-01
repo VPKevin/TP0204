e6d1165cdcbf41d871a84695b8c2a1a33d2b7862
Kevin <ipandore45@gmail.com>
2022-08-02 00:25:50 +0200

    update config drone

--- .drone/deploy.sh
+++ .drone/deploy.sh
@@ -2,5 +2,5 @@
 set -e
 
 docker ps
-docker-compose --project-name tp0204 down --rmi all
-docker-compose --project-name tp0204 up -d
+#docker-compose --project-name tp0204 down --rmi all
+#docker-compose --project-name tp0204 up -d
