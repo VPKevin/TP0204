7e50716b8d50893c1eae3204f6c4ff8c7c9aa3bb
Kevin <ipandore45@gmail.com>
2022-08-02 20:18:40 +0200

    update config drone

--- .drone/deploy.sh
+++ .drone/deploy.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 set -e
 
-docker-compose --project-name tp0204 down --rmi all
+lastimg = docker-compose --project-name tp0204 images -q
+docker-compose --project-name tp0204 pull
+docker-compose --project-name tp0204 down
 docker-compose --project-name tp0204 up -d
+docker rmi $lastimg --no-prune
+#docker-compose --project-name tp0204 down --rmi all
