902affb02fccbfe79e5fa6b8f4f4b56b7026049d
Kevin <ipandore45@gmail.com>
2022-08-03 19:41:10 +0200

    update config drone

--- .drone/deploy.sh
+++ .drone/deploy.sh
@@ -13,8 +13,8 @@ oldImg=$(docker-compose --project-name tp0204 images -q)
 #docker-compose --project-name tp0204 up -d
 printf "${GREEN}Step 5 :${NC} Suppressions des images orphelines\n"
 lastImg=$(docker-compose --project-name tp0204 images -q)
-#[[ " ${lastImg[*]} " =~ " fby4yk4364d4egr4cdt " ]] && echo "true" || echo "false"
-echo "${lastImg[@]}"
+[[ " ${lastImg[*]} " =~ " fby4yk4364d4egr4cdt " ]] && echo "true" || echo "false"
+#echo "${lastImg[@]}"
 #echo [[ "${lastImg[*]}" ]]
 #for i in $oldImg ; do
 #    if [[ ! " ${lastImg[*]} " =~ " $i " ]]; then
