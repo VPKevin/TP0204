3b5d180d82aa0a065abdb28965f91683a2263fd9
Kevin <ipandore45@gmail.com>
2022-08-03 17:54:20 +0200

    update config drone

--- .drone/deploy.sh
+++ .drone/deploy.sh
@@ -5,7 +5,7 @@ NC='\033[0m' # No Color
 
 printf "${GREEN}Step 1 :${NC} Recuperation des anciennes images utilisées\n"
 oldImg=$(docker-compose --project-name tp0204 images -q)
-printf oldImg
+printf $oldImg
 printf "${GREEN}Step 2 :${NC} Mise à jour des images\n"
 docker-compose --project-name tp0204 pull
 printf "${GREEN}Step 3 :${NC} Téléchargement des mises à jour des images\n"
