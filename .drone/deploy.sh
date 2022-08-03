554421d763eca190f6c37c28cc3bbed5add713de
Kevin <ipandore45@gmail.com>
2022-08-03 02:54:39 +0200

    update config drone

--- .drone/deploy.sh
+++ .drone/deploy.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 set -e
 
-lastImg=$(docker-compose --project-name tp0204 images -q)
+printf "${GREEN}Step 1 :${NC} Recuperation des anciennes images utilisées\n"
+oldImg=$(docker-compose --project-name tp0204 images -q)
+printf "${GREEN}Step 2 :${NC} Mise à jour des images\n"
 docker-compose --project-name tp0204 pull
+printf "${GREEN}Step 3 :${NC} Téléchargement des mises à jour des images\n"
 docker-compose --project-name tp0204 down
+printf "${GREEN}Step 4 :${NC} Démarrage des conteneurs\n"
 docker-compose --project-name tp0204 up -d
-docker rmi $lastImg --no-prune
+printf "${GREEN}Step 5 :${NC} Suppressions des images orphelines\n"
+lastImg=$(docker-compose --project-name tp0204 images -q)
+for i in $oldImg ; do
+    if [[ ! " ${array[*]} " =~ " ${i} " ]]; then
+        docker rmi $i --no-prune
+    fi
+done
+
 #docker-compose --project-name tp0204 down --rmi all
