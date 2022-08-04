c5967be8e2e374ede322862cc9afc896761f262c
Kevin <ipandore45@gmail.com>
2022-08-04 22:34:41 +0200

    update config drone

--- .drone/test.sh
+++ .drone/test.sh
@@ -13,7 +13,7 @@ echo "$BASH_VERSION"
 echo $BASH_VERSION
 echo ${GREEN}"testyx"
 
-Array1='key1 key2 key3 key4 key5 key6 key7 key8 key9 key10'
+Array1='key1 key2 key4 key6 key7 key8 key9 key10'
 Array2='key1 key2 key3 key4 key5 key6'
 
 Array3=
@@ -22,5 +22,5 @@ for i in $Array1; do
     for j in $Array2; do
         [ $i == $j ] && { skip=1; break; }
     done
-    [ $skip == "" ] || echo $i
+    [ $skip != "" ] || echo $i
 done
