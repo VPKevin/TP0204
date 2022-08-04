fe3a680be3f48ab72a5451778bf24ce715e4910f
Kevin <ipandore45@gmail.com>
2022-08-05 00:27:16 +0200

    update config drone

--- .drone/test.sh
+++ .drone/test.sh
@@ -17,22 +17,22 @@ Array1='key1 key2 key4 key6 key7 key8 key9 key10' # 2 7 8 9 10
 Array2='key1 key3 key4 key5 key6' # 3 5
 echo $Array1
 echo "$Array1"
-Array3=
-for i in $Array1; do
-    skip=0
-    for j in $Array2; do
-        [ $i == $j ] && { skip=1; break; }
-    done
-    [ $skip = 1 ] || echo $i
-done
+#Array3=
+#for i in $Array1; do
+#    skip=0
+#    for j in $Array2; do
+#        [ $i == $j ] && { skip=1; break; }
+#    done
+#    [ $skip = 1 ] || echo $i
+#done
 #ArrayEnd={ echo "$Array1" "$Array2" | tr ' ' '\n' | sort | uniq -u };
 #echo $ArrayEnd
 echo "$Array1" "$Array2" | tr ' ' '\n' | sort | uniq -u
-echo '\n'
+echo ' blax '
 echo "$Array1" "$Array2" | tr ' ' '\n' | sort | uniq -d
-echo '\n'
+echo ' blax '
 echo "$Array1" "$Array2" | tr ' ' '\n' | sort | uniq -u | uniq
-echo '\n'
+echo ' blax '
 echo "$Array1" "$Array2" | tr ' ' '\n' | sort | uniq -d | uniq
 
 #ArrayEnd=`echo "$Array1" "$Array2" | tr ' ' '\n' | sort | uniq -d | uniq`
