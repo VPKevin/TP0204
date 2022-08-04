b11bccddb3db938ffe3f496b04217af718a967e9
Kevin <ipandore45@gmail.com>
2022-08-04 22:13:04 +0200

    update config drone

--- .drone/test.sh
+++ .drone/test.sh
@@ -19,9 +19,8 @@ Array2='key1 key2 key3 key4 key5 key6'
 Array3=
 for i in $Array1; do
     skip=
-    for j in "${Array2[@]}"; do
-        [[ $i == $j ]] && { skip=1; break; }
+    for j in $Array2; do
+        [ $i == $j ] && { skip=1; break; }
     done
     [[ -n $skip ]] || echo $i
 done
-declare -p Array3
