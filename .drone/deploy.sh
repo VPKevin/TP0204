fe1d8ae6068e5c5d3461008ebcdf55bfd56e1e1a
Kevin <ipandore45@gmail.com>
2022-08-05 12:50:14 +0200

    update config drone

--- .drone/deploy.sh
+++ .drone/deploy.sh
@@ -4,18 +4,17 @@ GREEN='1;32'
 NC='\033[0m' # No Color
 
 dockerName=$DRONE_REPO_NAME'_'$DRONE_COMMIT_BRANCH
-echo $dockerName
-err=(dgferrstbr{vgfserrdbr})
-printf "Build ${build.number} of ${build.branch} started. ${build.link}"
+
+printf "Build $dockerName"
 printf "${GREEN}Step 1 :${NC} Récupération des anciennes images utilisées\n"
-oldImg=$(docker-compose --project-name tp0204 images -q)
+oldImg=$(docker-compose --project-name $dockerName images -q)
 printf "${GREEN}Step 2 :${NC} Téléchargement des mise à jours des images\n"
-docker-compose --project-name tp0204 pull
+docker-compose --project-name $dockerName pull
 printf "${GREEN}Step 3 :${NC} Redémarrage des conteneurs\n"
-docker-compose --project-name tp0204 down
-docker-compose --project-name tp0204 up -d
+docker-compose --project-name $dockerName down
+docker-compose --project-name $dockerName up -d
 printf "${GREEN}Step 4 :${NC} Suppressions des images orphelines\n"
-lastImg=$(docker-compose --project-name tp0204 images -q)
+lastImg=$(docker-compose --project-name $dockerName images -q)
 
 for i in $oldImg; do
     skip=0
